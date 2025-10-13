29b1ef2396eaddc94b84f528d89b5b7a78ab153d
Jimmy Lee <jimlee604again@gmail.com>
2025-10-12 18:10:39 -0700

    add todo project

---
+++ ToDoListTable/ToDoListTable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoListTable
+//
+//  Created by Jimmy Lee on 10/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ ToDoListTable/ToDoListTableTests/ToDoListTableTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListTableTests.swift
+//  ToDoListTableTests
+//
+//  Created by Jimmy Lee on 10/12/25.
+//
+
+import Testing
+@testable import ToDoListTable
+
+struct ToDoListTableTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
