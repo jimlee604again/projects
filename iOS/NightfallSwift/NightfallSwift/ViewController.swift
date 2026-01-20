2f4cac245ede876290c631fe23088f45cecb70d1
Jimmy Lee <jimlee604again@gmail.com>
2026-01-20 12:28:41 -0800

    starting nightfall swift, remove storyboard

---
+++ iOS/NightfallSwift/NightfallSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NightfallSwift
+//
+//  Created by Jimmy Lee on 1/20/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    self.view.backgroundColor = UIColor.blue
+  }
+
+}
+
