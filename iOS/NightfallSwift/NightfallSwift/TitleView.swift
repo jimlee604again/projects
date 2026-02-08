ae03df87f689a369746d76d1e8410c80076d82b2
Jimmy Lee <jimlee604again@gmail.com>
2026-02-07 20:33:21 -0800

    swift title screen no button function

--- iOS/NightfallSwift/NightfallSwift/TitleView.swift
+++ iOS/NightfallSwift/NightfallSwift/TitleView.swift
@@ -10,17 +10,49 @@ import UIKit
 
 class TitleView : UIView {
   private let titleText : UILabel
+  private let startButton : UIButton
   
   init() {
     self.titleText = UILabel()
     self.titleText.text = "Nightfall"
     self.titleText.textColor = .white
+    self.titleText.font = UIFont.boldSystemFont(ofSize: 40.0)
     
+    self.startButton = UIButton(type: .system)
+    var startButtonConfig = UIButton.Configuration.filled()
+    var attributedTitle = AttributedString("Start")
+    attributedTitle.font = UIFont.systemFont(ofSize: 20.0, weight: .bold)
+    startButtonConfig.attributedTitle = attributedTitle
+    startButtonConfig.baseForegroundColor = .black
+    self.startButton.configuration = startButtonConfig
+    
+    self.startButton.backgroundColor = .blue
+    self.startButton.layer.cornerRadius = 6.0
+    startButtonConfig.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 20)
+    self.startButton.configuration = startButtonConfig
+
     super.init(frame: CGRectZero)
-    
-//    self.backgroundColor = .black
-    self.backgroundColor = .red
+
+    self.backgroundColor = .black
     self.addSubview(self.titleText)
+    self.addSubview(self.startButton)
+
+    let titleSizeThatFits = titleText.sizeThatFits(self.bounds.size)
+    let buttonSizeThatFits = startButton.sizeThatFits(self.bounds.size)
+    
+    titleText.translatesAutoresizingMaskIntoConstraints = false
+    startButton.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      titleText.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+      titleText.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: -200),
+      titleText.widthAnchor.constraint(equalToConstant: titleSizeThatFits.width),
+      titleText.heightAnchor.constraint(equalToConstant: titleSizeThatFits.height),
+      
+      startButton.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+      startButton.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: 100),
+      startButton.widthAnchor.constraint(equalToConstant: buttonSizeThatFits.width),
+      startButton.heightAnchor.constraint(equalToConstant: buttonSizeThatFits.height)
+    ])
   }
   
   required init?(coder: NSCoder) {
--- iOS/NightfallSwift/NightfallSwift/ViewController.swift
+++ iOS/NightfallSwift/NightfallSwift/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
     self.view = TitleView()
   }
 
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
